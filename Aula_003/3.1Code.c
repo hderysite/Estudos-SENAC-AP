56ff0c3eea09382294fbab9a2485ebc76c9f6fe7
tenluvid <annapachevo890@gmail.com>
2025-02-19 17:45:59 -0300

    Exercício 1

--- Aula_003/3.1Code.c
+++ Aula_003/3.1Code.c
@@ -1,6 +1,15 @@
 int main()
 {
+    printf("MediaDasNotas ::::::: \n");
 
+    float nota1;
+    float nota2;
+    printf("Digite a sua primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a sua segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("A sua média é: %f", (nota1 + nota2)/2);
 
     return 0;
 }
