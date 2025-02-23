decf7e6e9bf48129a3235b07e84b5789fa494e24
tenluvid <annapachevo890@gmail.com>
2025-02-23 19:02:51 -0300

    Exercício 6

---
+++ Aula_003/3.6Code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int par = 0;
+  int impar = 1;
+  
+  int p;
+  printf("Digite um número par: \n");
+  scanf("%d", &p);
+  printf("O número é: %d\n", par);
+  
+  int i;
+  printf("Digite um número impar: \n");
+  scanf("%d", &i);
+  printf("O número é: %d\n", impar);
+
+  return 0;
+}
